e9f5791c8702e5dd451aa451997181fa1ca39846
MatteoGranata <matteogranata92@gmail.com>
2024-03-06 17:10:58 +0100

    added Thanks component

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Welcome from '../views/Welcome.vue';
+import Config from '../views/Config.vue';
+import Step1 from '../views/Step1.vue';
+import Step2 from '../views/Step2.vue';
+import Step3 from '../views/Step3.vue';
+import Thanks from '../views/Thanks.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/welcome',
+      name: 'welcome',
+      component: Welcome
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/config',
+      name: 'config',
+      component: Config,
+      children: [
+        {
+          path: '/step1',
+          name: 'step1',
+          component: Step1
+        },
+        {
+          path: '/step2',
+          name: 'step2',
+          component: Step2
+        },
+        {
+          path: '/step3',
+          name: 'step3',
+          component: Step3
+        },
+
+      ]
+    },
+    {
+      path: '/thanks',
+      name: 'thanks',
+      component: Thanks,
+    },
+    {
+      path: '/',
+      component: Welcome
+    },
   ]
 })
 
---
+++ src/views/Thanks.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="overflow">
+
+    <div class="container">
+      <div class="thanks-title">
+        <h1>
+          THANKS YOU SO MUCH
+        </h1>
+      </div>
+      <div class="thanks-container">
+        <div class="thanks">
+          <p>
+            Thank you for choosing Sunnee's Kids line and for being part of our sustainability mission. With your
+            support, we are helping to protect the oceans and educate future generations about the importance of
+            recycling and environmental conservation. Your customized thermal bottle is currently being prepared and
+            will soon be ready for shipment. Keep following Sunnee on our social channels to stay updated on new offers,
+            initiatives, and tips for a sustainable lifestyle. Thanks again for being a valuable member of the Sunnee
+            family! <br>
+            Stay sunny.
+          </p>
+        </div>
+      </div>
+      <div class="summary">
+        <h2>Summary of choices:</h2>
+        <div class="cap">
+          <p>
+            Cap:
+          </p>
+          <div class="color" :style="{ background: $route.query.capColor }">
+            <p>
+              {{ $route.query.capColor }}
+            </p>
+          </div>
+        </div>
+        <div class="body">
+          <p>
+            Body:
+          </p>
+          <div class="color" :style="{ background: $route.query.bodyColor }">
+            <p>
+              {{ $route.query.bodyColor }}
+            </p>
+          </div>
+        </div>
+        <div class="bottom">
+          <p>
+            Bottom:
+          </p>
+          <div class="color" :style="{ background: $route.query.bottomColor }">
+            <p>
+              {{ $route.query.bottomColor }}
+            </p>
+          </div>
+        </div>
+        Have you changed your mind?
+        <router-link to="/step1" class="link">Click me</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (!this.$route.query.capColor || !this.$route.query.bodyColor || !this.$route.query.bottomColor) {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<style>
+.thanks-title {
+  position: relative;
+  top: -10vw;
+  left: 4rem;
+  width: 90vw;
+  font-size: 6vw;
+}
+
+.thanks-container {
+  display: flex;
+  position: relative;
+  top: -20vw;
+  width: 100%;
+  justify-content: flex-start;
+}
+
+.thanks {
+  margin: 0 4rem;
+  display: flex;
+  width: 67vw;
+  align-items: start;
+  font-size: 2vw;
+}
+
+.summary {
+  width: 40px;
+  position: relative;
+  bottom: 20vw;
+  left: 4rem;
+  font-size: 1.3vw;
+  padding-bottom: 4rem;
+  width: 80vw;
+}
+
+.cap,
+.body,
+.bottom {
+  display: flex;
+}
+
+.color {
+  display: flex;
+  align-items: center;
+  align-self: center;
+  border-radius: 5px;
+  height: 2vh;
+  width: fit-content;
+  text-transform: uppercase;
+  text-align: center;
+  margin-left: 1rem;
+}
+
+@media (max-width: 980px) {
+  .overflow {
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .thanks,
+  .summary {
+    font-size: 4.5vw;
+    width: 100%;
+    left: 4vw;
+  }
+
+  .thanks {
+    margin-left: 4vw;
+    margin-right: 0;
+  }
+}
+</style>
